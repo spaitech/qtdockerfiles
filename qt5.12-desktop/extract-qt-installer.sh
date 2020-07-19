0d28da6653e8480228181cbc753919e1029176ad
Kalessin <vladkost.ua@gmail.com>
2020-07-19 14:20:41 +0300

    Fix for Qt install.

--- qt5.12-desktop/extract-qt-installer.sh
+++ qt5.12-desktop/extract-qt-installer.sh
@@ -12,5 +12,7 @@ export QT_VER=$(echo "${QT_VERSION}" | tr -d .)
 
 ./installer.run -platform minimal --verbose --script qt-installer-noninteractive.qs 
 
+find "$QT_PATH" -mindepth 1 -maxdepth 1 ! -name "${QT_VERSION}" -exec echo 'Cleaning Qt SDK: {}' \; -exec rm -r '{}' \;
+
 ./install-linuxdeployqt.sh
 rm -rf /tmp/qt
